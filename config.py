b0c032bf3867b3ed3b98250139fd2d5dd14c3db9
Wei Ye <vejuhust@gmail.com>
2015-05-10 16:44:35 +0800

    Update config.py for new dev

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Configurations for github profile crawler"""
+"""Configurations for snippet crawler"""
 
 from multiprocessing import cpu_count
 from time import strftime
@@ -7,15 +7,15 @@ from time import strftime
 
 config_db_addr  = "127.0.0.1"
 config_db_port  = 27017
-config_db_name  = "gitcrawl"
+config_db_name  = "snippetcrawl"
 config_db_user  = "YOUR_USERNAME"
 config_db_pass  = "YOUR_PASSWORD"
 
-config_db_profile   = "profile"
+config_db_snippet   = "snippet"
 config_queue_crawl  = "queue_crawl"
 config_queue_page   = "queue_page"
 
-config_log_file     =  "dev_{}.log".format(strftime("%Y-%m-%d"))
+config_log_file     =  "sc_{}.log".format(strftime("%Y-%m-%d"))
 
 config_crawl_process    = 4
 config_crawl_retry      = 3
@@ -24,12 +24,5 @@ config_crawl_timeout    = 5
 
 config_idle_sleep       = 1
 
-config_parse_domain     = "https://github.com"
+config_parse_domain     = "http://neihanshequ.com"
 config_parse_process    = max(4, cpu_count())
-
-config_report_folder    = "charts"
-config_report_interval  = 60
-config_report_item      = 16
-config_report_port      = 8090
-config_report_status    = "status.json"
-config_report_step      = 2
