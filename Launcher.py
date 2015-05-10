4fc85699d82493a3c9f77b3917ef223c83d8fcd6
Wei Ye <vejuhust@gmail.com>
2015-05-11 03:59:45 +0800

    Make max_time in launching urls configurable

--- Launcher.py
+++ Launcher.py
@@ -12,25 +12,25 @@ from time import sleep
 
 
 urls = [
-    "http://neihanshequ.com/joke/?is_json=1&max_time=1431285365",       # 首页
+    "http://neihanshequ.com/joke/?is_json=1&max_time=",       # 首页
 
-    "http://neihanshequ.com/bar/1/?is_json=1&max_time=1431285365",      # 内涵段子
-    "http://neihanshequ.com/bar/11/?is_json=1&max_time=1431285365",     # 精辟语录
-    "http://neihanshequ.com/bar/76/?is_json=1&max_time=1431285365",     # 友问友答
-    "http://neihanshequ.com/bar/80/?is_json=1&max_time=1431285365",     # 来自世界的恶意
-    "http://neihanshequ.com/bar/82/?is_json=1&max_time=1431285365",     # 机智如我
-    "http://neihanshequ.com/bar/59/?is_json=1&max_time=1431285365",     # 突然觉得哪里不对
-    "http://neihanshequ.com/bar/5/?is_json=1&max_time=1431285365",      # 我的xx是极品
+    "http://neihanshequ.com/bar/1/?is_json=1&max_time=",      # 内涵段子
+    "http://neihanshequ.com/bar/11/?is_json=1&max_time=",     # 精辟语录
+    "http://neihanshequ.com/bar/76/?is_json=1&max_time=",     # 友问友答
+    "http://neihanshequ.com/bar/80/?is_json=1&max_time=",     # 来自世界的恶意
+    "http://neihanshequ.com/bar/82/?is_json=1&max_time=",     # 机智如我
+    "http://neihanshequ.com/bar/59/?is_json=1&max_time=",     # 突然觉得哪里不对
+    "http://neihanshequ.com/bar/5/?is_json=1&max_time=",      # 我的xx是极品
 
-    "http://neihanshequ.com/bar/25/?is_json=1&max_time=1431285365",     # 治愈系英文
-    "http://neihanshequ.com/bar/26/?is_json=1&max_time=1431285365",     # 精选书摘
-    "http://neihanshequ.com/bar/3/?is_json=1&max_time=1431285365",      # 早晚必读的话
-    "http://neihanshequ.com/bar/53/?is_json=1&max_time=1431285365",     # 旅行家
-    "http://neihanshequ.com/bar/46/?is_json=1&max_time=1431285365",     # 佛家禅语
-    "http://neihanshequ.com/bar/49/?is_json=1&max_time=1431285365",     # 悸动歌词
-    "http://neihanshequ.com/bar/69/?is_json=1&max_time=1431285365",     # 图写心情
-    "http://neihanshequ.com/bar/51/?is_json=1&max_time=1431285365",     # 情感点滴
-    "http://neihanshequ.com/bar/60/?is_json=1&max_time=1431285365",     # 耽美宅腐
+    "http://neihanshequ.com/bar/25/?is_json=1&max_time=",     # 治愈系英文
+    "http://neihanshequ.com/bar/26/?is_json=1&max_time=",     # 精选书摘
+    "http://neihanshequ.com/bar/3/?is_json=1&max_time=",      # 早晚必读的话
+    "http://neihanshequ.com/bar/53/?is_json=1&max_time=",     # 旅行家
+    "http://neihanshequ.com/bar/46/?is_json=1&max_time=",     # 佛家禅语
+    "http://neihanshequ.com/bar/49/?is_json=1&max_time=",     # 悸动歌词
+    "http://neihanshequ.com/bar/69/?is_json=1&max_time=",     # 图写心情
+    "http://neihanshequ.com/bar/51/?is_json=1&max_time=",     # 情感点滴
+    "http://neihanshequ.com/bar/60/?is_json=1&max_time=",     # 耽美宅腐
 ]
 
 
@@ -55,7 +55,8 @@ class Launcher():
 
     def add_urls_to_queue_crawl(self, urls):
         with closing(DatabaseAccessor()) as dal:
-            for url in urls:
+            for url_prefix in urls:
+                url = url_prefix + str(config_crawl_date_max)
                 print("add {} - {}".format(url, dal.queue_crawl_create(url)))
 
 
--- config.py
+++ config.py
@@ -21,6 +21,8 @@ config_crawl_process    = min(1, cpu_count())
 config_crawl_retry      = 3
 config_crawl_sleep      = 2
 config_crawl_timeout    = 5
+config_crawl_date_min   = 5
+config_crawl_date_max   = 1431285365
 
 config_idle_sleep       = 1
 
